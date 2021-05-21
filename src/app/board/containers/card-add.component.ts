914221ac6444a7c6426ee2ae32ae748bd1463a67
icepeng <typ0@naver.com>
2021-05-21 18:08:15 +0900

    feat: add card 문구 변경
    resolve #6

--- src/app/board/containers/card-add.component.ts
+++ src/app/board/containers/card-add.component.ts
@@ -2,11 +2,13 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { BoardPageActions } from '../actions';
+import * as fromBoard from '@monster/board/reducers';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-card-add',
   templateUrl: './card-add.component.html',
-  styleUrls: ['./card.component.scss', './card-add.component.scss']
+  styleUrls: ['./card.component.scss', './card-add.component.scss'],
 })
 export class CardAddComponent implements OnInit {
   @Input() listId!: string;
@@ -16,6 +18,11 @@ export class CardAddComponent implements OnInit {
   formGroup = new FormGroup({
     title: new FormControl('', [Validators.required]),
   });
+
+  isListEmpty$ = this.store.select(fromBoard.selectAllCards).pipe(
+    map((cards) => cards.filter((card) => card.listId === this.listId)),
+    map((arr) => arr.length === 0)
+  );
 
   constructor(private store: Store) {}
 
